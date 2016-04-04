0f2d42b27470801120c8deb18a5a796db1d80f5c
Soyn <w604408482@163.com>
2016-04-04 21:30:08 +0800

    delete meanless import

--- src/test.py
+++ src/test.py
@@ -5,9 +5,5 @@
 from modules import spider
 from modules import search_engine
 
-from BeautifulSoup import BeautifulSoup, SoupStrainer
-import requests
-
 url = "https://github.com/Soyn"
 index, graph = spider.CrawlWeb(url, 2)
-
